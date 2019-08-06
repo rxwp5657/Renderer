8389990334ce1feb677620ed39771efacc3671e6
Juan Carlos Sanchez Ruiz <charles@Juans-MacBook-Pro-2.local>
2019-08-06 13:54:47 -0500

    file structure changes

--- renderer/project.clj
+++ renderer/project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [uncomplicate/fluokitten "0.9.1"]]
+  :java-source-paths ["src/render/comp/tree"]
   :main ^:skip-aot render.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
